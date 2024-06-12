7a37d43d03e99ce8e4a91b9532b2cfc949af59b9
George Adams <george.adams02@imperial.ac.uk>
2024-06-12 21:53:28 +0100

    update gradient calculation for wgan

--- 001_2D_DBGAN/utils/wgan_utils.py
+++ 001_2D_DBGAN/utils/wgan_utils.py
@@ -13,10 +13,83 @@ from torchvision.utils import save_image
 import pathlib as pathlib
 
 
+# def calculate_gradient_penalty(model, real_images, fake_images, device):
+#     """Calculates the gradient penalty loss for WGAN GP"""
+#     # Random weight term for interpolation between real and fake data
+#     alpha = torch.randn((real_images.size(0), 1, 1, 1), device=device)
+#     # Get random interpolation between real and fake data
+#     interpolates = (alpha * real_images + ((1 - alpha) * fake_images)).requires_grad_(True)
+
+#     model_interpolates = model(interpolates)
+#     grad_outputs = torch.ones(model_interpolates.size(), device=device, requires_grad=False)
+
+#     # Get gradient w.r.t. interpolates
+#     gradients = torch.autograd.grad(
+#         outputs=model_interpolates,
+#         inputs=interpolates,
+#         grad_outputs=grad_outputs,
+#         create_graph=True,
+#         retain_graph=True,
+#         only_inputs=True,
+#     )[0]
+#     gradients = gradients.view(gradients.size(0), -1)
+#     gradient_penalty = torch.mean((gradients.norm(2, dim=1) - 1) ** 2)
+#     return gradient_penalty
+
+
+
+
+
+
+# def calculate_gradient_penalty(self, real_images, fake_images):
+#     eta = torch.FloatTensor(self.batch_size,1,1,1).uniform_(0,1)
+#     eta = eta.expand(self.batch_size, real_images.size(1), real_images.size(2), real_images.size(3))
+#     if self.cuda:
+#         eta = eta.cuda(self.cuda_index)
+#     else:
+#         eta = eta
+
+#     interpolated = eta * real_images + ((1 - eta) * fake_images)
+
+#     if self.cuda:
+#         interpolated = interpolated.cuda(self.cuda_index)
+#     else:
+#         interpolated = interpolated
+
+#     # define it to calculate gradient
+#     interpolated = Variable(interpolated, requires_grad=True)
+
+#     # calculate probability of interpolated examples
+#     prob_interpolated = self.D(interpolated)
+
+#     # calculate gradients of probabilities with respect to examples
+#     gradients = autograd.grad(outputs=prob_interpolated, inputs=interpolated,
+#                            grad_outputs=torch.ones(
+#                                prob_interpolated.size()).cuda(self.cuda_index) if self.cuda else torch.ones(
+#                                prob_interpolated.size()),
+#                            create_graph=True, retain_graph=True)[0]
+
+#     # flatten the gradients to it calculates norm batchwise
+#     gradients = gradients.view(gradients.size(0), -1)
+    
+#     grad_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean() * self.lambda_term
+#     return grad_penalty
+
+
+
+
+
+
+
 def calculate_gradient_penalty(model, real_images, fake_images, device):
     """Calculates the gradient penalty loss for WGAN GP"""
     # Random weight term for interpolation between real and fake data
-    alpha = torch.randn((real_images.size(0), 1, 1, 1), device=device)
+    #alpha = torch.randn((real_images.size(0), 1, 1, 1), device=device)
+    alpha = torch.FloatTensor(real_images.size(0),1,1,1).uniform_(0,1)
+    alpha = alpha.expand(real_images.size(0), real_images.size(1), real_images.size(2), real_images.size(3))
+
+    alpha = alpha.to(device)
+
     # Get random interpolation between real and fake data
     interpolates = (alpha * real_images + ((1 - alpha) * fake_images)).requires_grad_(True)
 
@@ -35,6 +108,31 @@ def calculate_gradient_penalty(model, real_images, fake_images, device):
     gradients = gradients.view(gradients.size(0), -1)
     gradient_penalty = torch.mean((gradients.norm(2, dim=1) - 1) ** 2)
     return gradient_penalty
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
